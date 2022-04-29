4d9a1bce8efc7a6db8ad700dcd819f54c50bc295
Karim Romagnani <karim.romagnani@gmail.com>
2022-04-29 07:29:06 +0200

    property example

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import 'tachyons'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Hello />
+    <Hello greeting={"Hello " + "React Ninja" }/>
   </React.StrictMode>
 );
 
