13feac37eb144fd77f80ad14bcf722acd5114449
Karim Romagnani <karim.romagnani@gmail.com>
2022-05-07 09:36:13 +0200

    added css changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import { robots } from './robots';
+import "./App.css";
 
 
 class App extends Component{
@@ -14,14 +15,14 @@ class App extends Component{
     }
 
     onSearchChange = (event) =>{
-        this.setState({searchfield: event.target.value});                
+        this.setState({searchfield: event.target.value});
     }
 
     render(){    
         const filteredRobots = this.state.robots.filter(robots =>{
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         });
-        console.log(filteredRobots);
+        
         return (
             <div className="tc">
                 <h1>Robofriends</h1>
